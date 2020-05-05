acb96b4cab25b1940b2bb6370caf687d31f42928
abrahamnarvaezv@gmail.com <abrahamnarvaezv@gmail.com>
2020-05-04 19:20:49 -0500

    53 - Rutas con parametros - Router... Continuar en: 54 - Recibiendo parametros por URL - ActivatedRoute

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,14 @@ import { FormsModule } from '@angular/forms';
 import { APP_ROUTING } from './app.routes'
 
 // Servicios
-
+import { HeroesService } from './servicios/heroes.service';
 // Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +31,9 @@ import { HeroesComponent } from './components/heroes/heroes.component';
 //    HttpModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
